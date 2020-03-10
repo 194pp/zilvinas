67bbc124fcc575ecb2899694950a05d58dd4d218
livanauskas <ivanauskulukas@gmail.com>
2020-03-10 20:01:03 +0200

    Palindromai

--- src/com/kcs/example/FourthExample.java
+++
@@ -1,7 +0,0 @@
-package com.kcs.example;
-
-public class FourthExample {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/second/SecondTasks.java
@@ -0,0 +1,29 @@
+package second;
+
+import java.util.Scanner;
+
+public class SecondTasks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+
+
+        String text = "KOL EINU ŠUNIE LOK".replaceAll(" ", "");
+
+        boolean isPalindrome = true;
+        for(int i = 0; i < text.length(); i++){
+            char first = text.charAt(i);
+            char last = text.charAt(text.length()-i-1);
+            if (first != last){
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome){
+            System.out.println("Polindromas");
+        }
+        else{
+            System.out.println("Nepolindromas");
+        }
+    }
+}
